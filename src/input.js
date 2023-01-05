36b7ac9bee138ba071c431adbe32251550dc45d3
Booi <56810550+booimangang@users.noreply.github.com>
2023-01-05 14:13:06 +0530

    Add auto suggestion which only include input val

--- src/input.js
+++ src/input.js
@@ -2,33 +2,55 @@ import { getGeoCoordURL, getGeoCoords } from './fetch'
 
 function input() {
 
-    let geo = 'delhi';
-    let limit = 5;
-
     const inputField = document.querySelector('.input-field')
     const geoSuggestionEl = document.querySelector('.geo-suggestions')
     // console.log(inputField)
 
-    inputField.addEventListener('input', async (e) => {
-        let urls = getGeoCoordURL(e.target.value, limit)
+
+    const removeSuggestionEl = function (val) {
+        let suggestions = document.querySelector('.geo-suggestions')
+        suggestions.childNodes.forEach(suggestion => {
+            let text = suggestion.innerHTML.replace(/(<([^>]+)>)/ig, '')
+            if (!(text.toLowerCase().includes(val.toLowerCase()))) {
+                console.log(suggestion)
+                suggestion.remove()
+            }
+        });
+        console.log(suggestions.childElementCount)
+        if (suggestions.childElementCount > 6) {
+            suggestions.removeChild(suggestions.lastChild)
+        }
+    }
+
+    const createSuggestionEl = function name(val, geoSuggestion) {
+        let suggestion = document.createElement('div')
+        suggestion.classList.add()
+        // console.log(geoSuggestion.name)
+        suggestion.innerHTML = '<strong>' + geoSuggestion.name.substr(0, val.length) + '</strong>'
+        suggestion.innerHTML += geoSuggestion.name.substr(val.length)
+        geoSuggestionEl.prepend(suggestion)
+    }
+
+    const inputAutoComplete = async function (val, suggestionsArr) {
+
+        for (let i = 0; i < suggestionsArr.length; i++) {
+            if (suggestionsArr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                createSuggestionEl(val, suggestionsArr[i])
+                removeSuggestionEl(val)
+            }
+        }
+    }
+
+    const handleInput = async function (e) {
+        let limit = 5;
+        let val = e.target.value
+        let urls = getGeoCoordURL(val, limit)
         let coords = await getGeoCoords(urls)
         // console.log(coords)
-        coords.forEach(coord => {
-            let suggestion = document.createElement('div')
-            suggestion.innerText = coord.name
-            geoSuggestionEl.prepend(suggestion)
-            if (geoSuggestionEl.childElementCount > 6) {
-                geoSuggestionEl.removeChild(geoSuggestionEl.lastChild)
-            }
-
-            console.log(geoSuggestionEl.childElementCount)
-        });
-    })
-
-
-    const inputAutoComplete = function (inp, arr) {
-
+        inputAutoComplete(val, coords)
     }
+
+    inputField.addEventListener('input', handleInput)
 
 }
 
