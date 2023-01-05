fe7d36c9532b8827a0c4969ddd79900170fb24bf
Booi <56810550+booimangang@users.noreply.github.com>
2023-01-05 10:27:28 +0530

    Add API fetch funtion

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const API_KEY = 'c613a13b184358c24caf13e21b9f03f0'\r\n\r\nconst url = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}'\r\n\r\nconst getCoor = function () {\r\n\r\n}\r\n\r\nconst getCoordinate = async function (url) {\r\n    const response = await fetch(url);\r\n    console.log(response)\r\n\r\n}\r\n\r\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
